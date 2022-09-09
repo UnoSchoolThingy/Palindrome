8881d6a9c14bb9ac749623e15651fff76ce6e7d6
UnoSchoolThingy <113061893+UnoSchoolThingy@users.noreply.github.com>
2022-09-09 13:14:20 -0700

    Make it check the case and stuff

--- palindrome.cpp
+++ palindrome.cpp
@@ -26,7 +26,7 @@ int main() {
     int idx = 0;
     char s2[80];
     for (int i = 0; i < strlen(s); i++) {
-      if (isalnum(s[i])) s2[idx++] = s[i];
+      if (isalnum(s[i])) s2[idx++] = tolower(s[i]); // If it's a number tolower just returns the number 
     }
     s2[idx] = 0; // zero terminate 
     strcpy(s, s2); // copy string
